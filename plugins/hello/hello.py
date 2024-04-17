9963c074107e1d9d42c7349a0c9f5d44a0e6a0df
zhaoshenchen1988 <zhaoshenchen1988@gmail.com>
2024-04-17 18:00:40 +0800

    noting

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -98,12 +98,16 @@ class Hello(Plugin):
             e_context["reply"] = reply
             e_context.action = EventAction.BREAK_PASS  # 事件结束，并跳过处理context的默认逻辑
 
-        if content == "Hi":
+        if content in ["Hi", "hi", "Hello", "hello"]:
             reply = Reply()
             reply.type = ReplyType.TEXT
-            reply.content = "Hi"
+            msg: ChatMessage = e_context["context"]["msg"]
+            if e_context["context"]["isgroup"]:
+                reply.content = f"Hi Mua~ {msg.actual_user_nickname} from {msg.from_user_nickname}"
+            else:
+                reply.content = f"Hi Mua~ {msg.from_user_nickname}"
             e_context["reply"] = reply
-            e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑，一般会覆写reply
+            e_context.action = EventAction.BREAK_PASS  # 事件结束，进入默认处理逻辑，一般会覆写reply
 
         if content == "End":
             # 如果是文本消息"End"，将请求转换成"IMAGE_CREATE"，并将content设置为"The World"
